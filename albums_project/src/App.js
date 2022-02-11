c1d3914a9e62ce9c679ff82779e0da92538170e4
Selim Emre Genel <seg348@drexel.edu>
2022-02-10 22:20:22 -0500

    Imported and used the mockFetchHelper() function to retrieve the data from albums.json

--- albums_project/src/App.js
+++ albums_project/src/App.js
@@ -1,20 +1,19 @@
 import './App.css';
+import { mockFetchHelper } from './mock_api';
+import React, { useState } from "react";
 
 function App() {
+  const sendData = require("./albums.json");
+  const [data, setData] = useState(null);
+  mockFetchHelper(true, sendData).then(responseData => setData(responseData));
+
+  console.log(data);
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
